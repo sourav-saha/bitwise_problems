3ca111671916dc5d64413cbb103212e271eb1a9c
sourav-saha <sourav.saha048@gmail.com>
2022-01-14 00:44:41 +0530

    Create reset_last_k_bits.py

---
+++ reset_last_k_bits.py
@@ -0,0 +1,22 @@
+'''
+Given two integers n and k. Reset (set to zero) the last k bits in the number n, and print the result. It is recommended to find solution without loops.
+
+Input
+Two numbers n (0 ≤ n ≤ 231 - 1) and k (0 ≤ k ≤ 30).
+
+Output
+Print the result of resetting k last bits.
+
+Input example #1 content_copy
+5 1
+Output example #1 content_copy
+4
+'''
+
+def reset_bits(n, k)->int:
+	return n & ~((1<<k)-1)
+ 
+ 
+if __name__ == "__main__":
+	n, k = map(int, input().strip().split(' '))
+	print(reset_bits(n, k))
